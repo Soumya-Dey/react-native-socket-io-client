15823c6320a849849462f24fcb3110d8ff352260
Soumya Dey <soumyadey200@hotmail.com>
2021-04-27 00:09:14 +0530

    minor updates

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import axios from 'axios';
 
-const socket = io('https://1bfb5f4e01c6.ngrok.io');
+const socket = io('https://e14070b8ff37.ngrok.io');
 
 export default function App() {
-  const [time, setTime] = useState("no data");
+  const [data, setData] = useState({ todaysCollection: 'no data' });
 
   useEffect(() => {
+    const requestData = async () => {
+      console.log('making a request to backend');
+      const { data } = await axios({
+        method: 'get',
+        url: 'http://134.209.153.159:9000/api/user/collectionAndDue',
+        headers: {
+          'auth-token':
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjAzNTQxNzk0YzZjMTMzNTU4NGUwZmRjIn0sImlhdCI6MTYxOTQ2MDU0OH0.nyTQnf1F0k2LBRhF_XoMJU7yXKuOrSQ7AZi6YQs-iGk',
+        },
+      });
+
+      setData(data);
+    };
+
     socket.on('connect', () => {
       console.log(`connection establised: ID[${socket.id}]`);
     });
 
-    socket.on('test', (data) => {
-      setTime(data);
+    socket.on('reset-collection', (msg) => {
+      console.log(msg);
+      requestData();
     });
 
     socket.on('disconnect', () => {
@@ -24,8 +40,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{time}</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{data.todaysCollection.toString()}</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -35,9 +51,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
