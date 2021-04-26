68b26d0f6afb76303407c55ee4638f7dabbc204d
Soumya Dey <soumyadey200@hotmail.com>
2021-04-26 12:50:25 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import io from 'socket.io-client'
+
+const socket = io('http://09aa1d0dfe0b.ngrok.io');
 
 export default function App() {
+  const [time, setTime] = useState("no data");
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log(`connection establised: ID[${socket.id}]`);
+    });
+
+    socket.on('test', (data) => {
+      setTime(data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`connection terminated`);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{time}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +35,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    fontSize: 24
+  }
 });
